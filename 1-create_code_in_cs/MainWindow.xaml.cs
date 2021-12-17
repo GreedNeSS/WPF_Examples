5aaedeba949b1bb0fcec4479d105d1253c7f6e52
GreedNeSS <ruslan199126@gmail.com>
2021-12-17 18:13:59 +0300

    Добавьте файлы проекта.

---
+++ 1-create_code_in_cs/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1_create_code_in_cs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitGrid();
+        }
+
+        public void InitGrid()
+        {
+            Grid grid = new Grid();
+
+            grid.ColumnDefinitions.Add(new ColumnDefinition()
+            { Width = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition()
+            { Width = new GridLength(2, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
+
+            Border border = new Border();
+            border.Background = new SolidColorBrush(Colors.Red);
+            Grid.SetColumn(border, 1);
+            Grid.SetRow(border, 1);
+            grid.Children.Add(border);
+
+            Content = grid;
+        }
+    }
+}
---
+++ 2-events/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2_events
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string firstname = Firstname.Text;
+            string lastname = Lastname.Text;
+
+            if (firstname == "Введите свое имя" ||
+                lastname == "Введите свою фамилию")
+            {
+                MessageBox.Show("Вы не заполнили поля!");
+            }
+            else
+            {
+                MessageBox.Show("Ваше имя: " + lastname + " " + firstname);
+
+                Firstname.Text = "Введите свое имя";
+                Lastname.Text = "Введите свою фамилию";
+            }
+
+        }
+
+        private void Firstname_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Firstname.Text == "Введите свое имя")
+            {
+                Firstname.Text = "";
+            }
+        }
+
+        private void Firstname_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Firstname.Text == "")
+            {
+                Firstname.Text = "Введите свое имя";
+            }
+        }
+    }
+}
