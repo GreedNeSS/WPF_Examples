74b774dc8d50ab79c665c6fd45c44a53102e8812
GreedNeSS <ruslan199126@gmail.com>
2021-12-18 18:21:49 +0300

    Used DataContext, Binding and Models

---
+++ 3-data/Model/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace _3_data.Model
+{
+    public class Person: INotifyPropertyChanged
+    {
+        string firstname = "Иван";
+        public string Firstname
+        {
+            get { return firstname; }
+            set
+            {
+                firstname = value;
+                OnPropertyChanged("Firstname");
+            }
+        }
+
+        string lastname = "Иванов";
+        public string Lastname
+        {
+            get { return lastname; }
+            set
+            {
+                lastname = value;
+                OnPropertyChanged("Lastname");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
